a6056557ad076cb2fbf7659866c330842cda5c0b
Kshirsagar-Govind <88773342+Kshirsagar-Govind@users.noreply.github.com>
2022-09-24 22:27:59 +0530

    new changes

--- src/Components/Containers/[ Container ]cardScroller.jsx
+++ src/Components/Containers/[ Container ]cardScroller.jsx
@@ -1,31 +1,48 @@
-import React, { Component } from "react";
-import "../CSS/master-css.scss";
+import React, { Component, useEffect, useState } from "react";
+import { connect, Connect } from "react-redux";
+import { useHistory } from "react-router-dom";
 
-export default class CardScroller extends Component {
+import "../CSS/master-css.scss";
+import { getAllProduct } from "../../Services/Actions/[ Product ] getAllProductsData";
+
+class CardScroller extends Component {
   constructor(props) {
     super(props);
     this.state = {
+      products: [],
       type: "laptop",
     };
   }
   componentDidMount() {
-    this.setState({ type: "laptop" });
+    this.props.getAllProductsData();
+
+    if (this.props.all_products.data !== undefined) {
+      console.log(this.props.all_products.data);
+      this.setState({
+        products: this.props.all_products.data,
+      });
+    }
+  }
+
+  componentDidUpdate(nextProps) {
+    if (this.props !== nextProps) {
+      this.setState({
+        products: this.props.all_products.data,
+      });
+    }
   }
 
   render() {
     return (
       <div>
         <div className="scroller-wrapper">
-          <ScrollingCard />
-          <ScrollingCard />
-          <ScrollingCard />
-          <ScrollingCard />
-          <ScrollingCard />
-          <ScrollingCard />
-          <ScrollingCard />
-          <ScrollingCard />
-          <ScrollingCard />
-          <ScrollingCard />
+          {this.state.products.length > 0 ? (
+            this.state.products.map((item, index) => (
+              <ScrollingCard data={item} />
+            ))
+          ) : (
+            "No Products"
+          )}
         </div>
       </div>
     );
@@ -33,15 +50,50 @@ export default class CardScroller extends Component {
 }
 
 export const ScrollingCard = ({ data }) => {
+  const [ img, setImg ] = useState();
+  const [ link, setLink ] = useState(window.location.href);
+  let history = useHistory();
+
+  useEffect(
+    () => {
+      console.log(window.location.href);
+      setImg(data.product_images[0].imgURL);
+      setLink(window.location.href);
+    },
+    [ img, link ]
+  );
+
+  const show = () => {
+    window.open(`/item-view-page/${data.product_id}`);
+  };
+
   return (
     <div>
-      <div className="scrolling_card_wrapper">
-        <div className="img-section" />
+      <div className="scrolling_card_wrapper" onClick={show}>
+        <div className="img-section">
+          <img src={img} alt="" />
+        </div>
         <div className="footer-section">
-          <h1>Item Name - </h1>
-          <h1>2500/- </h1>
+          <h1>{data.product_Name} </h1>
+          <h1>{data.product_Price} </h1>
         </div>
       </div>
     </div>
   );
 };
+
+const mapStateToProps = state => {
+  return {
+    all_products: state.AllProducts,
+  };
+};
+
+const mapDispatchToProps = dispatch => {
+  return {
+    getAllProductsData: () => {
+      dispatch(getAllProduct());
+    },
+  };
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(CardScroller);
